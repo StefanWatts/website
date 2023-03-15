6e39fdffdd5616d41a4174e6b189314a999ebd90
StefanWatts <126801436+StefanWatts@users.noreply.github.com>
2023-03-15 11:35:20 +0800

    Initial

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict';
+//provides errors
+
+const switcher = document.querySelector('.btn');
+//pulls css .btn reference and links to JS switcher
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+//adds event handler (function) for click event
+     const className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Dark";
+    } else {
+        this.textContent = "Light";
+    }
+//if statement determines what to do if already on certain theme
+
+console.log('current class name: ' + className);
+//adds hidden message in webpage that can be read in developer tools - tells developer what theme is currently running
+});
+//
