283f384ccc66eb9d99d4d5766cbcc8ab4d8cc564
StefanWatts <126801436+StefanWatts@users.noreply.github.com>
2023-04-11 11:47:52 +0800

    v25

--- script.js
+++ script.js
@@ -20,3 +20,16 @@ console.log('current class name: ' + className);
 //adds hidden message in webpage that can be read in developer tools - tells developer what theme is currently running
 });
 //
+
+
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
